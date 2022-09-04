da66df54a3d74ffbd6f353d24e05f7d0d8d2f107
obitastico <obit-obit@web.de>
2022-09-04 23:58:54 +0200

    Moved Models to Models Folder

---
+++ DeezerDownloader.Core/Models/Album.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace DeezerDownloader.Core
+{
+    public class Album
+    {
+        [JsonProperty(PropertyName = "id")]
+        public long Id { get; set; }
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+        [JsonProperty(PropertyName = "tracklist")]
+        public string Tracklist { get; set; }
+
+        public override string ToString()
+        {
+            return $"<Album Id={Id} Title={Title} Tracklist={Tracklist}>";
+        }
+    }
+}
